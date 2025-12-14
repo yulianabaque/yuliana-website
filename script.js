9cab021afa06629fa5f9bec6dda342ae4f9dc2f8
yuliana163 <giulianabaque@gmail.com>
2025-12-14 18:09:51 +0100

    btn

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const detalles = document.getElementById("cvDetalles");
 
 boton.addEventListener("click", () => {
   detalles.classList.toggle("oculto");
+
+  if (detalles.classList.contains("oculto")) {
+    boton.textContent = "Mostrar detalles";
+  } else {
+    boton.textContent = "Ocultar detalles";
+  }
 });
