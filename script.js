981b988e3e48c3d431ea41ed5631c2e06af8cf57
yuliana163 <giulianabaque@gmail.com>
2026-01-22 12:26:29 +0100

    img

--- script.js
+++ script.js
@@ -4,11 +4,33 @@ const detalles = document.getElementById("cvDetalles");
 boton.addEventListener("click", () => {
   detalles.classList.toggle("oculto");
 
-  if (detalles.classList.contains("oculto")) {
-    boton.textContent = "Mostrar detalles";
-  } else {
-    boton.textContent = "Ocultar detalles";
-  }
+  boton.textContent = detalles.classList.contains("oculto")
+    ? "Equip Loadout"
+    : "Unequip Loadout";
 });
 
+const crosshair = document.getElementById("crosshair");
 
+document.addEventListener("mousemove", (e) => {
+  crosshair.style.left = e.clientX + "px";
+  crosshair.style.top = e.clientY + "px";
+});
+const muzzle = document.getElementById("muzzle-flash");
+
+document.addEventListener("click", (e) => {
+  muzzle.style.left = e.clientX + "px";
+  muzzle.style.top = e.clientY + "px";
+
+  muzzle.style.opacity = "1";
+  muzzle.style.transform =
+    "translate(-50%, -50%) scale(1.4) rotate(" +
+    (Math.random() * 30 - 15) +
+    "deg)";
+
+  setTimeout(() => {
+    muzzle.style.opacity = "0";
+    muzzle.style.transform = "translate(-50%, -50%) scale(0)";
+  }, 70);
+
+  
+});
